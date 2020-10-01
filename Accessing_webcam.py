04d1dd3eb0e77bdaf10fef4bf820c64c88d6f752
ashish-max <touchtoashish2001@gmail.com>
2020-10-01 15:21:49 +0530

    Create Accessing_webcam.py

---
+++ Accessing_webcam.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, cam = cap.read()
+
+    cv2.imshow('video',cam)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+    
+print("Web-Cam Released")
+
+cv2.destroyAllWindows()
+cap.release()
